3b27228da57b3fa0acf551bce5ff33a9c53407e7
Praveen Kumar <183719173+praveenpkv@users.noreply.github.com>
2024-12-18 16:10:44 +0530

    added modulus, cube and square

--- calculator.js
+++ calculator.js
@@ -13,3 +13,15 @@ const multiply = function (a, b) {
 const divison = function (dividend, divisor) {
   return dividend / divisor;
 }
+
+const modulus = function (dividend, divisor) {
+  return dividend % divisor;
+}
+
+const square = function (number) {
+  return number * number;
+}
+
+const cube = function (number) {
+  return number * number * number;
+}
