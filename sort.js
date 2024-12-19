901e7c208af8340e929239c46948690f2b5d0616
Praveen Kumar <183719173+praveenpkv@users.noreply.github.com>
2024-12-19 14:47:41 +0530

    sorted odd even

--- sort.js
+++ sort.js
@@ -6,65 +6,71 @@ const isOdd = function (number) {
   return (number & 1) === 1;
 }
 
-[34,59,20,12,8,1,97].sort(function(x, y) {
+const sortOddEven = function (array) {
+  return array.sort(function(x, y) {
 	if(isOdd(x) && isEven(y)) return -1;
 	if(isEven(x) && isOdd(y)) return 1;
 	return x - y;
 })
+}
 
 //  +++++++++++++   SECOND +++++++++
 
-[[1,2],[0,8],[9,10]].sort(function(x, y) {
+const sortArrayWRTFirstElement = function (array) {
+  return array.sort(function(x, y) {
   return x[0] - y[0];
 })
-
-
-// ++++++++++++++ THIRD ++++++++++++
-
-
-const array = [[1], [8, 6, 7], [2, 3, 5, 1]];
-
-array.sort(function(x, y) {
-	x.sort(function (x, y) { return x - y});
-	y.sort(function (x, y) { return x - y});
-  return x[0] - y[0];
-})
-
-//  ++++++++++++ Fourth ++++++++
-
-const array = [[1], [8, 6, 7], [2, 3, 5, 1]];
-
-const sorting = function (element) {
-  return element.sort(function (x, y) {return x - y;})
 }
 
-array.map(sorting);
+// // ++++++++++++++ THIRD ++++++++++++
 
 
-//  +++++++++++  sort string with length
+// const array = [[1], [8, 6, 7], [2, 3, 5, 1]];
 
-const string = ["praveen", "ku", "verma", "shivam"];
+// array.sort(function(x, y) {
+// 	x.sort(function (x, y) { return x - y});
+// 	y.sort(function (x, y) { return x - y});
+//   return x[0] - y[0];
+// })
 
-string.sort(function (x, y) {
-if(x.includes("r")) return -1;
-if(y.includes("r")) return -1;
-            return x.length - y.length;
-})
+// //  ++++++++++++ Fourth ++++++++
 
+// const array = [[1], [8, 6, 7], [2, 3, 5, 1]];
 
-if(x.includes("r")) return -1;
-if(y.includes("r")) return -1;
+// const sorting = function (element) {
+//   return element.sort(function (x, y) {return x - y;})
+// }
 
-
-
-
-
-
+// array.map(sorting);
 
 
-nst string = ["praveen", "shiv ji", "ku", "verma", "shivam"];
+// //  +++++++++++  sort string with length
 
-string.sort(function (x, y) {
-            if(x.includes("r")) return -1;
-})
-[ "verma", "praveen", "shiv ji", "ku", "shivam" ]
+// const string = ["praveen", "ku", "verma", "shivam"];
+
+// string.sort(function (x, y) {
+// if(x.includes("r")) return -1;
+// if(y.includes("r")) return -1;
+//             return x.length - y.length;
+// })
+
+
+// if(x.includes("r")) return -1;
+// if(y.includes("r")) return -1;
+
+
+
+
+
+
+
+
+// nst string = ["praveen", "shiv ji", "ku", "verma", "shivam"];
+
+// string.sort(function (x, y) {
+//             if(x.includes("r")) return -1;
+// })
+// [ "verma", "praveen", "shiv ji", "ku", "shivam" ]
+
+console.log(sortOddEven([34,59,20,12,8,1,97]));
+console.log(sortArrayWRTFirstElement([[1,2],[0,8],[9,10]]));
